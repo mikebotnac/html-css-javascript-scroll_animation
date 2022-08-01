e1836b2a9a4b75dc3339daf9557523965061677c
Rajel718 <superdev718@gmail.com>
2022-08-01 18:33:59 +0900

    animation completed

--- js/index.js
+++ js/index.js
@@ -1,16 +1,33 @@
+var screenh = window.innerHeight;
+var toph = document.querySelector('.topDev').clientHeight;
+var h = document.querySelector('.main').clientHeight;
 var i = 0;
-var h = document.body.scrollHeight;
-addEventListener('scroll', () => {
-  var y = window.scrollY;
-  if (y == 908) { i = 1 }
-  if (i == 0) {
-    document.getElementById('Cashew').style.top = `calc(40% - ${334 - 140 * y / h}px)`;
-    document.getElementById('Cashew').style.transform = `rotate(${-90 + 90 * y / h}deg)`;
-    document.getElementById('Cheese2').style.top = `calc(40% - ${218 - 40 * y / h}px)`;
-    document.getElementById('Cheese2').style.transform = `rotate(${-30 + 30 * y / h}deg)`;
-    document.getElementById('Cashew2').style.top = `calc(40% - ${137 - 55 * y / h}px)`;
-    document.getElementById('Cracker').style.top = `calc(40% + ${152 - 100 * y / h}px)`;
-    document.getElementById('Cracker').style.transform = `rotate(${90 - 90 * y / h}deg)`;
-    document.getElementById('Bread').style.top = `calc(40% + ${403 - 180 * y / h}px)`;
+var y = toph;
+
+function animation() {
+  if (y > toph) { i = 1; }
+  if (y>(toph-screenh) && y<=toph && i == 0) {
+    document.getElementById('Cashew').style.top = `calc(40% - ${192 + 58 * (toph - y) / toph}px)`;
+    document.getElementById('Cashew').style.transform = `rotate(${-90 + 90 * y / toph}deg)`;
+    document.getElementById('Cheese2').style.top = `calc(40% - ${175 + 25 * (toph - y) / toph}px)`;
+    document.getElementById('Cheese2').style.transform = `rotate(${-30 + 30 * y / toph}deg)`;
+    document.getElementById('Cashew2').style.top = `calc(40% - ${85 + 25 * (toph - y) / toph}px)`;
+    document.getElementById('Cracker').style.top = `calc(40% + ${53 + 17 * (toph - y) / toph}px)`;
+    document.getElementById('Cracker').style.transform = `rotate(${90 - 90 * y / toph}deg)`;
+    document.getElementById('Bread').style.top = `calc(40% + ${223 + 47 * (toph - y) / toph}px)`;
   }
+  if (y > (toph-screenh)) {
+    document.getElementById('Cashew').style.transition = "0.5s";
+    document.getElementById('Cheese2').style.transition = "0.5s";
+    document.getElementById('Cashew2').style.transition = "0.5s";
+    document.getElementById('Cracker').style.transition = "0.5s";
+    document.getElementById('Bread').style.transition = "0.5s";
+  }
+}
+
+animation();
+
+addEventListener('scroll', () => {
+  y = window.scrollY;
+  animation();
 });
